c23d941015806a9b9bcc72a0f003f75503adc8b6
Jenniffer Macharia <jenn.macharia@gmail.com>
2023-07-06 17:33:28 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
-        Weather  App
-            </div>
+      Weather App
+      <footer>
+        This project coded by Jenn
+        <a href="https://creative-gecko-bbc428.netlify.app/" target="blank">
+          Website
+        </a>
+      </footer>
+    </div>
   );
 }
 
