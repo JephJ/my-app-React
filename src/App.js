a1cd9eb183ee4f51c6fe70191127a1b8b33ac89a
Jenniffer Macharia <jenn.macharia@gmail.com>
2023-07-07 10:12:35 -0500

    Weather css and weather App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
-
+import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project coded by Jenn
-        <a href="https://creative-gecko-bbc428.netlify.app/" target="blank">
-          Website
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <Weather />
+        <footer>
+          This project coded by Jenn
+          <a href="https://creative-gecko-bbc428.netlify.app/" target="blank">
+            Website
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
